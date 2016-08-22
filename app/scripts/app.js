215d6d91874d6cedbd63e3f0f03ef081c1782d05
Sharmi <rai.sharmistha@gmail.com>
2016-08-22 15:28:24 -0400

    configure the module with providers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,8 @@
-angular.module('blocJams', ['ui.router']);
+(function() {
+    function config($stateProvider, $locationProvider) {
+    }
+
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
