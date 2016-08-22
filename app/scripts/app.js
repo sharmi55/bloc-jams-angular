46241ac29446dc149eebff869b1d13a0d440f0b2
Sharmi <rai.sharmistha@gmail.com>
2016-08-22 16:33:37 -0400

    configure paths with

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,11 @@
 (function() {
     function config($stateProvider, $locationProvider) {
-    }
+      $locationProvider
+        .html5Mode({
+          enabled: true,
+          requireBase: false
+        });
+}
 
     angular
         .module('blocJams', ['ui.router'])
