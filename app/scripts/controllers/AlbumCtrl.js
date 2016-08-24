178ffbe9b91bc35a2875851c1085a0f605af4c29
Sharmi <rai.sharmistha@gmail.com>
2016-08-24 14:55:45 -0400

    add albumData property that holds copy of albumPicasso

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,5 +1,10 @@
-(function)() {
+(function() {
   function AlbumCtrl() {
       this.albumData = [];
+      this.albumData.push(angular.copy(albumPicasso));
   }
-}
+
+  angular
+    .module('blocJams')
+    .controller('AlbumCtrl', AlbumCtrl);
+})();
