95cafdb3f8c0326537dbf08bedacff01ce15e247
Sharmi <rai.sharmistha@gmail.com>
2016-09-01 09:26:38 -0400

    link directive logic to DOM

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -3,7 +3,11 @@
     return {
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
-      restrict: 'E'
+      restrict: 'E',
+      scope: { },
+      link: function(scope, element, attributes) {
+        // directive logic to return
+      }
     };
   }
   angular
