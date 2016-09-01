fe9c4d0733ed388e065b5a182a4cfb319cf4f85c
Sharmi <rai.sharmistha@gmail.com>
2016-09-01 11:14:38 -0400

    set playback position of a song

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -148,6 +148,17 @@
         }
       };
 
+/*
+* @function setCurrentTime
+* @desc Set current time (in seconds) of currently playing song
+* @param {Number} time
+*/
+      SongPlayer.setCurrentTime = function(time) {
+          if (currentBuzzObject) {
+              currentBuzzObject.setTime(time);
+          }
+      };
+
         return SongPlayer;
     }
 
