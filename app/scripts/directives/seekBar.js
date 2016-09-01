4dd0818428db0b76a813fd7c3996719f44315c25
Sharmi <rai.sharmistha@gmail.com>
2016-09-01 09:14:40 -0400

    Create custom directive seek bar

---
+++ app/scripts/directives/seekBar.js
@@ -0,0 +1,12 @@
+(function() {
+  function seekBar() {
+    return {
+      templateUrl: '/templates/directives/seek_bar.html',
+      replace: true,
+      restrict: 'E'
+    };
+  }
+  angular
+    .module('blocJams')
+    .directive('seekBar', seekBar);
+}) ();
